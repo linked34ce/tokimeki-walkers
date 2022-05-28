ae506e36897dd47fd64ebf66df1e17a76a6a52a4
linked34ce <9797ouki@gmail.com>
2022-05-28 11:33:23 +0900

    template

--- hello.py
+++ hello.py
@@ -1,11 +1,12 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h1>Hello, World!</h1>"
+    return render_template("hello.html", name="World")
 
 @app.route("/<name>")
 def hello_user(name):
-    return "Hello, {}!".format(name)
+    return render_template("hello.html", name=name)
