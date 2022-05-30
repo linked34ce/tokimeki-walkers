b45edaadc5362deb834d5ba810b8f5a8e558d3d3
linked34ce <9797ouki@gmail.com>
2022-05-30 13:22:25 +0900

    database refreshed

--- create_table.py
+++ create_table.py
@@ -7,7 +7,7 @@ conn = sqlite3.connect(dbname)
 cur = conn.cursor()
 
 sqls = [
-   #"create table Blog (id integer primary key, title varchar(50) not null, body varchar(500) not null, created_at datetime not null default current_timestamp);"
+   "create table Blog (id integer primary key, title varchar(50) not null, body varchar(500) not null, created_at datetime not null default current_timestamp);", 
    "create table Users (id integer primary key, name varchar(50) not null unique, password varchar(25));"
 ]
 
