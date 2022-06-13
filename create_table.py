26a9d5a8360571d9779fbcb86ad4d98c0aa67e36
linked34ce <9797ouki@gmail.com>
2022-06-13 13:45:25 +0900

    a little fixed

--- create_table.py
+++ create_table.py
@@ -7,9 +7,9 @@ conn = sqlite3.connect(dbname)
 cur = conn.cursor()
 
 sqls = [ 
-   "create table Users (id integer primary key, name varchar(50) not null unique, password varchar(25));"
-   # "create table Locations (id integer primary key, name verchar(50) not null unique, latitude double not null, longitude double not null, summary varchar(500) not null, scene varchar(500) not null, image varchar(100) not null);",
-   # "create table Visits (username varchar(50) not null, location_id int not null, photo varchar(100) not null, visit_count int not null default 0, last_visit datettime not null default current_timestamp, primary key(username, location_id));"
+   "create table Users (id integer primary key, name varchar(64) not null unique, password varchar(128) not null, profile varchar(1024));",
+   "create table Locations (id integer primary key, name verchar(64) not null unique, latitude double not null, longitude double not null, summary varchar(1024) not null, scene varchar(1024) not null, image varchar(128) not null);",
+   "create table Visits (username varchar(64) not null, location_id int not null, photo varchar(128) not null, time datettime not null default current_timestamp);"
 ]
 
 for sql in sqls:
