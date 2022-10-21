d5d9fcb78719e59917d416a6a7a7cb58b2515e88
linked34ce <9797ouki@gmail.com>
2022-10-21 12:23:20 +0900

    location names fixed

--- app.py
+++ app.py
@@ -699,5 +699,5 @@ def logout():
     return redirect(url_for("login"))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80, debug=False, threaded=True)
-    # app.run(host="0.0.0.0", port=80, debug=True, threaded=True)
+    # app.run(host="0.0.0.0", port=80, debug=False, threaded=True)
+    app.run(host="0.0.0.0", port=80, debug=True, threaded=True)
